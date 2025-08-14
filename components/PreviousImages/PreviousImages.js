802bd86e2ba9290c62564b50570fc1289e87fca8
dilpreet_singh_gill <dilpreetg0707@gmail.com>
2025-08-14 20:27:19 +0530

    Fixed : Dependencies

--- components/PreviousImages/PreviousImages.js
+++ components/PreviousImages/PreviousImages.js
@@ -1,8 +1,8 @@
 import React, { useState, useCallback } from "react";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
+import Gallery from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 import { Container } from "react-bootstrap";
-import AnimatedText from "@/components/AnimatedText/AnimatedText";
 import useScrollReveal from "@/hooks/useScrollReveal";
 import { prevImgsDetails } from "@/util/config";
 import Heading from "../Heading/Heading";
@@ -10,45 +10,34 @@ import Heading from "../Heading/Heading";
 const PreviousImages = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const revealContainer = useScrollReveal();
 
-  const openLightbox = useCallback((event, { photo, index }) => {
+  const openLightbox = useCallback((event, { index }) => {
     setCurrentImage(index);
     setViewerIsOpen(true);
   }, []);
 
   const closeLightbox = () => {
-    setCurrentImage(0);
     setViewerIsOpen(false);
   };
-  const revealContainer = useScrollReveal();
 
   return (
     <Container ref={revealContainer} as="section" id="glimpses">
-      {/* <div className="headings_glittered">
-                <AnimatedText>
-                    <h1>Glimpses</h1>
-                </AnimatedText>
-            </div> */}
       <div className="headings_glittered">
         <Heading>Glimpses</Heading>
       </div>
+
       <Gallery photos={prevImgsDetails} onClick={openLightbox} />
-      <Container>
-        <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <Carousel
-                currentIndex={currentImage}
-                views={photos.map((x) => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title,
-                }))}
-              />
-            </Modal>
-          ) : null}
-        </ModalGateway>
-      </Container>
+
+      <Lightbox
+        open={viewerIsOpen}
+        close={closeLightbox}
+        index={currentImage}
+        slides={prevImgsDetails.map((img) => ({
+          src: img.src,
+          alt: img.title,
+        }))}
+      />
     </Container>
   );
 };
